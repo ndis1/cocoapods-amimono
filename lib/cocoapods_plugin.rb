d2c9d5f2a726841628a70852a72d18778177d15c
Nick DiStefano <nickd@tumblr.com>
2017-04-28 07:58:47 -0400

    try excluding share target

--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -5,6 +5,7 @@ Pod::HooksManager.register('cocoapods-amimono', :post_install) do |installer_con
   # We exclude all targets that contain `Test`, which might not work for some test targets
   # that doesn't include that word
   pods_targets = installer_context.umbrella_targets.reject { |target| target.cocoapods_target_label.include? 'Test' }
+  pods_targets = pods_targets.reject { |target| target.cocoapods_target_label.include? 'Share' }
   updater = Amimono::BuildPhasesUpdater.new
   updater.update_build_phases(installer_context, pods_targets)
 end
